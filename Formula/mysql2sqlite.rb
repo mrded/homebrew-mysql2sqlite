fcda08543ec859af8854112558fd4caeb005a33e
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-07-19 00:53:50 +0100

    Update formula

--- Formula/mysql2sqlite.rb
+++ Formula/mysql2sqlite.rb
@@ -2,8 +2,9 @@ require 'formula'
 
 class Mysql2Sqlite < Formula
   homepage ''
-  url 'https://raw.github.com/mrded/mysql2sqlite/master/bin/mysql2sqlite'
-  sha1 '19dc0364d681f5a2f26dc72991f59bc0e2fcf0cc'
+  head 'https://github.com/mrded/mysql2sqlite.git', :revision => '03b2d4d60df8242c96a39fa1b68135fc20ce3f61'
+
+  depends_on :sqlite 
 
   def install
     prefix.install 'mysql2sqlite'
